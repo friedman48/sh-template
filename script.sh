871038b86527507d30791a42f5738010039abd97
friedman48 <p4n7h3rmail@proton.me>
2024-10-21 17:30:05 +0300

    Added script.sh

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# File: <filename>.sh
+# Author: <Author Name> <email@example.com>
+# Created on: <Date>
+#
+# Description: <Brief description of what this script does>
+#
+# Copyright <Year> <Author Name>  
+#
+# Disclaimer:
+# This software is provided "as is," without warranty of any kind, express or implied,
+# including but not limited to the warranties of merchantability, fitness for a particular
+# purpose, and noninfringement. In no event shall the authors or copyright holders be liable
+# for any claim, damages, or other liability, whether in an action of contract, tort, or
+# otherwise, arising from, out of, or in connection with the software or the use or other
+# dealings in the software.
+#
+
